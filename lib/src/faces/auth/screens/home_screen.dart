f75862c545e46f3a739c6a257fd04c2cd9f27a98
abdurrahimagca <agcaabdurrahim@outlook.com>
2024-11-13 11:47:00 +0300

    removed grpc for non necesseary reqs

--- lib/src/faces/auth/screens/home_screen.dart
+++ lib/src/faces/auth/screens/home_screen.dart
@@ -6,9 +6,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:grpc/grpc.dart' as grpc;
-import 'package:lyricfy/src/internal/services/generated/lyricFetch.pb.dart';
-import 'package:lyricfy/src/internal/services/generated/lyricFetch.pbenum.dart';
-import 'package:lyricfy/src/internal/services/generated/lyricFetch.pbgrpc.dart';
 import 'package:lyricfy/src/faces/auth/screens/auth_flow_start_screen.dart';
 import 'package:lyricfy/src/internal/apis/middles/tokenValidate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -43,26 +40,6 @@ class HomeScreen extends StatelessWidget {
       dev.log('home screen Request failed: ${e.message}');
       return;
     }
-    final channel = grpc.ClientChannel("10.0.2.2",
-        port: 50051,
-        options: const grpc.ChannelOptions(
-            credentials: grpc.ChannelCredentials.insecure()));
-    final stub = lyricFetchServiceClient(channel);
-    List<Future<void>> lyricReq = tops.take(10).map((song) async {
-      // Your async code here
-      try {
-        final response = await stub.getLyric(LyricFetchRequest(
-          title: song.name,
-          artist: song.artist,
-        ));
-        dev.log(
-            '********* ${song.name} Lyric************************\n: ${response.lyrics}');
-      } catch (e) {
-        dev.log('Lyric Request failed: ${e}');
-      }
-    }).toList();
-    await Future.wait(lyricReq);
-    await channel.shutdown();
   }
 
   void _sout(context) async {
